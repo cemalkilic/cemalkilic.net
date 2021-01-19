c19c3a6497d3656a3b4d8a5bbbb1544a14be934e
Cemal Kılıç <cemalkilic96@gmail.com>
2021-01-19 23:22:50 +0300

    Deploy only public folder

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -4,16 +4,13 @@ set -x
 
 if [ $TRAVIS_BRANCH == "master" ] ; then
     
-    # initialize the git repo and push it to server
+    # initialize the git repo in public folder and push it to server
+    cd public
     git init
     
     git remote add deploy "deploy@deploy.cemalkilic.net:/var/www/cemalkilic.net"
     git config user.name "Travis CI"
     git config user.email "cemalkilic96+travis@gmail.com"
-   
-    # Do not deploy the unnecassary files
-    rm -f .gitignore
-    cp travis/.deployignore .gitignore 
 
     git add .
     git commit -m "Deploy"
